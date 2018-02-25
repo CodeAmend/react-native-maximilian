be45341e4ad04287a841bae03702d2bae01dcfb0
Michael Bruce Allen <CodeAmend@Gmail.Com>
2018-02-25 12:36:27 -0500

    Add SharePlaces, FindPlaces with tabs and icons

--- src/screens/Auth/index.js
+++ src/screens/Auth/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import startMainTabs from '../MainTabs/startMainTabs';
 
-const AuthScreen = () =>
-  <View>
-    <Text>Auth Screen!</Text>
-  </View>
+const AuthScreen = () => {
+  const handleLogin = () => {
+    startMainTabs();
+  };
+
+  return (
+    <View>
+      <Text>Auth Screen!</Text>
+      <Button title="Login" onPress={handleLogin}/>
+    </View>
+  )
+};
 
 export default AuthScreen;
