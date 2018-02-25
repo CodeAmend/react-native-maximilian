10681d78b552794de29ca8ebf2491dca74034957
Michael Bruce Allen <CodeAmend@Gmail.Com>
2018-02-25 16:50:22 -0500

    Add width and background color to drawer

--- src/screens/SideDrawer/index.js
+++ src/screens/SideDrawer/index.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text, Dimensions } from 'react-native';
+import { View, Text, Dimensions, StyleSheet } from 'react-native';
 
 class SideDrawer extends Component {
 
   render() {
 
     return (
-      <View style={{ width: Dimensions.get('window').width * 0.8 }}>
+      <View style={[
+        { width: Dimensions.get('window').width * 0.8 },
+        styles.container
+        ]}
+      >
         <Text>SideDrawer</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 22,
+    backgroundColor: 'white',
+    flex: 1
+  }
+})
+
 
 export default SideDrawer;
